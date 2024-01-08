b22c7f483b1effff1462b3055f83f18d5e7613ea
ndikintum <ndikintumrodrique2@gmail.com>
2024-01-08 03:20:52 +0100

    animation

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,7 @@ function checkBoxes () {
   boxes.forEach(box => {
     const boxTop = box.getBoundingClientRect().top
 
-    if(boxTop < triggerBottom) {
+    if (boxTop < triggerBottom) {
       box.classList.add('show')
     } else {
       box.classList.remove('show')
